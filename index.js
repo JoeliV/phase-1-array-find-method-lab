8b030af0816a63b9b0a7750533aa39fbfe6e15a0
Joeli Vasquez <joeliv106@gmail.com>
2023-02-01 02:39:55 -0500

    Adds index.js

--- index.js
+++ index.js
@@ -1,1 +1,14 @@
-// code your solution here
+
+/*function superBowlWin(record) {
+    const result = record.find(el => el.result === "W");
+    return result ? result.year : undefined;
+}
+*/
+
+const superbowlWin = (record) => {
+    let winningYear = record.find(d => d.result === "W"); 
+    return winningYear ? winningYear.year : undefined; 
+}
+ 
+
+
